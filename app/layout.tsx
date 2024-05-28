59ea4f4ee381b01cc2c319db4b2678bfc23bb99f
vaibdix <dixitvaib@gmail.com>
2024-05-28 10:21:51 +0530

    feat : added loading top bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,9 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
 import prismadb from "@/lib/prismadb";
 import { ToastProvider } from "@/providers/toast-providers";
+import NextTopLoader from "nextjs-toploader";
 
-import { ThemeProvider } from "@/providers/theme-providers"
+import { ThemeProvider } from "@/providers/theme-providers";
 
 const DMSns = DM_Sans({ subsets: ["latin"] });
 
@@ -21,22 +22,36 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={DMSns.className}>
           <ThemeProvider
-              attribute="class"
-              defaultTheme="dark"
-              enableSystem
-              disableTransitionOnChange
-            >
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
             <ToastProvider />
             <ModalProvider />
+            <NextTopLoader
+              color="#ea580c"
+              initialPosition={0.08}
+              crawlSpeed={200}
+              height={3}
+              crawl={true}
+              showSpinner={true}
+              easing="ease"
+              speed={200}
+              shadow="0 0 10px #ea580c,0 0 5px #ea580c"
+              template='<div class="bar" role="bar"><div class="peg"></div></div>
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+              zIndex={1600}
+              showAtBottom={false}
+            />
             {children}
           </ThemeProvider>
-          </body>
+        </body>
       </html>
     </ClerkProvider>
   );
